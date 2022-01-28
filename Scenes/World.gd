0bcfd7e65424df56fa7d30c6b0d9bf16c211601a
eggsnham07 <eggsnham07@gmail.com>
2022-01-28 16:07:09 -0700

    Remove crap

--- Scenes/World.gd
+++ Scenes/World.gd
@@ -2,21 +2,6 @@ extends Node
 
 export var EnemyScene: PackedScene
 
-var moving_left:bool = false
-var moving_right:bool = false
-
-func _process(delta):
-	var lr_i
-	
-	if moving_left == true and moving_right == true: lr_i = 0
-	elif moving_right == true: lr_i = 1
-	elif moving_left == true: lr_i = -1
-	
-	var new_position = $Player.position.x + lr_i * delta * $Player.speed
-	var half_width = $Player/Sprite.texture.get_width() / 2
-	
-	$Player.position.x = clamp(new_position, half_width, 180 - half_width)
-
 func _on_SpawnTimer_timeout():
 	for i in 3:
 		var enemy = EnemyScene.instance()
@@ -38,8 +23,3 @@ func _on_Player_died():
 
 func _on_ExplosionSound_finished():
 	get_tree().change_scene("res://Scenes/Menu.tscn")
-		
-
-
-func _on_MoveLeft_pressed():
-	pass # Replace with function body.
